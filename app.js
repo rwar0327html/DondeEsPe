6fc56de385f0668f1c1bab5a61eed4156bef2769
rwar0327html <rwar0327@gmail.com>
2025-11-14 22:56:15 -0500

    app.js

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-console.log("DondeEsPe conectado correctamente.");
-
 document.addEventListener("DOMContentLoaded", () => {
-  const estado = document.getElementById("estado-firebase");
-
-  try {
-    // Verificar si firebase ya está cargado (usando tu script global)
-    if (typeof firebase !== "undefined") {
-      estado.textContent = "✅ Firebase conectado correctamente.";
-      console.log("Firebase cargado:", firebase);
-    } else {
-      estado.textContent = "❌ Firebase NO está cargado.";
-      console.error("No se encontró Firebase.");
-    }
-  } catch (error) {
-    estado.textContent = "❌ Error conectando Firebase.";
-    console.error(error);
-  }
+  alert("DOM CARGADO");
 });
