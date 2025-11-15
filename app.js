2a74c2af5c3ea9f3d749e10047981791e42332e0
rwar0327html <rwar0327@gmail.com>
2025-11-14 20:35:37 -0500

    app.js

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-console.log("DondeEsPe conectado correctamente.");
-
-// Esperar a que cargue todo el DOM
 document.addEventListener("DOMContentLoaded", () => {
-  const estado = document.getElementById("estado-firebase");
-
-  // Probar si firebase realmente está cargado
-  try {
-    // Intentar acceder a firebase.app()
-    const testApp = firebase.app();
-
-    // Si no explota, Firebase está cargado
-    estado.textContent = "✅ Firebase conectado correctamente.";
-    console.log("Firebase cargado:", testApp);
-
-  } catch (error) {
-    // Si explota, Firebase NO cargó
-    estado.textContent = "❌ Firebase NO está cargado.";
-    console.error("Error:", error);
-  }
+  document.getElementById("estado-firebase").textContent =
+    "Firebase conectado correctamente.";
+  console.log("DondeEsPe conectado correctamente.");
 });
