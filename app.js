f3a3c097670debfc0105a75b924448b78c592fc4
rwar0327html <rwar0327@gmail.com>
2025-11-18 03:49:25 -0500

    app.js

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 mapboxgl.accessToken = "pk.eyJ1IjoiZG9uZGVlc3BlMi0iLCJhIjoiY21pNDgxOGZ2MGRubzJrbzV2Y3AyNmU1biJ9.rJ6hW4zLyWqk3a-m3IuHkQ";
 
-const map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v12",   // MAPA BLANCO
-  center: [-77.042793, -12.046374],
-  zoom: 12
-});
-
-/* VARIABLES */
 let selectedLat = null;
 let selectedLng = null;
 
-/* MOSTRAR MAPA */
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12", // MAPA BLANCO 2D (tipo Google)
+  center: [-77.042793, -12.046374], // Lima
+  zoom: 12
+});
+
+/* BAJAR AL MAPA CUANDO EL USUARIO PRESIONA EL BOTÓN */
 document.getElementById("verMapaBtn").onclick = () => {
-  const mapDiv = document.getElementById("map");
+  document.getElementById("map").scrollIntoView({ behavior: "smooth" });
 
-  mapDiv.style.display = "block";
-
+  // pequeño retraso para asegurar que el mapa se ajuste bien
   setTimeout(() => {
     map.resize();
   }, 200);
-
-  mapDiv.scrollIntoView({ behavior: "smooth" });
 };
 
-/* MODAL */
-const modal = document.getElementById("partyModal");
-document.getElementById("closeFormBtn").onclick = () => modal.style.display = "none";
-
-/* SLIDER */
-const slider = document.getElementById("eventoMin");
-const sliderValue = document.getElementById("sliderValue");
-slider.oninput = () => sliderValue.innerText = slider.value + " personas";
-
-/* ABRIR MODAL AL CLIC DEL MAPA */
+/* ABRIR FORMULARIO AL CLIC DEL MAPA */
 map.on("click", (e) => {
   selectedLat = e.lngLat.lat;
   selectedLng = e.lngLat.lng;
 
-  modal.style.display = "flex";
+  document.getElementById("partyModal").style.display = "flex";
 });
+
+/* CERRAR FORMULARIO */
+document.getElementById("closeFormBtn").onclick = () => {
+  document.getElementById("partyModal").style.display = "none";
+};
+
+/* SLIDER (ya funcionando fino) */
+const slider = document.getElementById("eventoMin");
+const sliderValue = document.getElementById("sliderValue");
+
+slider.oninput = () => {
+  sliderValue.innerText = slider.value + " personas";
+};
